f7edb345f3d793ab10daf57e538c082b58f47092
darkbtf <darkbtf@gmail.com>
2013-11-28 10:37:12 +0800

    sidebar

---
+++ src/com/taipeihot/jazzlist/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.taipeihot.jazzlist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // TODO: implement login
+        System.out.println("login");
+        Intent intent = new Intent();
+        intent.setClass(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+}
