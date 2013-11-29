c626ac1d6e7ac2618e90d9a48fdac5f0530007ec
DarkBtf <darkbtf@gmail.com>
2013-11-30 02:04:11 +0800

    sliding menu

--- src/com/taipeihot/jazzlist/LoginActivity.java
+++ src/com/taipeihot/jazzlist/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.taipeihot.jazzlist;
 
-import com.taipeihot.jazzlist.model.Data;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+
+import com.taipeihot.jazzlist.model.Data;
 
 public class LoginActivity extends Activity {
 
@@ -25,31 +25,36 @@ public class LoginActivity extends Activity {
         getMenuInflater().inflate(R.menu.login, menu);
         return true;
     }
-    
+
     public void login(View view) {
         // TODO: implement login
+        System.out.println("login");
         Intent intent = new Intent();
-        intent.setClass(LoginActivity.this, MainActivity.class);
-    	startActivity(intent);
+        //intent.setClass(LoginActivity.this, MainActivity.class);
+        intent.setClass(LoginActivity.this, LeftAndRightActivity.class);
+        System.out.println("meow");
+        startActivity(intent);
+        System.out.println("meow2");
     }
 
     public void registerLater(View view) {
-    	Intent intent = new Intent();
-        intent.setClass(LoginActivity.this, MainActivity.class);
-    	startActivity(intent);
+        Intent intent = new Intent();
+        //intent.setClass(LoginActivity.this, MainActivity.class);
+        intent.setClass(LoginActivity.this, LeftAndRightActivity.class);
+        startActivity(intent);
     }
-    
+
     private void writeAccount(){
-		SharedPreferences pre = getSharedPreferences("loginValue",MODE_PRIVATE);
-		String account = "david942j@gmail.com";
-		String nickname = "david";
-		String password = "Hue Nguyen";  
-		pre.edit().putString("account", account).putString("nickname",nickname).putString("password",Util.MD5(password)).commit();
+        SharedPreferences pre = getSharedPreferences("loginValue",MODE_PRIVATE);
+        String account = "david942j@gmail.com";
+        String nickname = "david";
+        String password = "Hue Nguyen";
+        pre.edit().putString("account", account).putString("nickname",nickname).putString("password",Util.MD5(password)).commit();
     }
     private void readAccount(){
-		SharedPreferences sp = getSharedPreferences("loginValue", MODE_PRIVATE);  
+        SharedPreferences sp = getSharedPreferences("loginValue", MODE_PRIVATE);
         String account = sp.getString("account", null);
-        String nickname = sp.getString("nickname", null);  
+        String nickname = sp.getString("nickname", null);
         String password = sp.getString("password", null);
         Data.setAccount(account,nickname,password);
     }
