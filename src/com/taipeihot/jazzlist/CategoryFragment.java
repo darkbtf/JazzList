82e0de493403b35d3bc046c53191a72eb32bb556
DarkBtf <darkbtf@gmail.com>
2013-12-01 02:09:17 +0800

    so tired

--- src/com/taipeihot/jazzlist/CategoryFragment.java
+++ src/com/taipeihot/jazzlist/CategoryFragment.java
@@ -1,23 +1,19 @@
- package com.taipeihot.jazzlist;
+package com.taipeihot.jazzlist;
 
 import android.annotation.SuppressLint;
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 @SuppressLint("NewApi")
 public class CategoryFragment extends Fragment {
-	
-	public CategoryFragment(){}
-	
-	@Override
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.fragment_category, container, false);
-         
-        return rootView;
+
+        return inflater.inflate(R.layout.fragment_category, null);
     }
 }
--- src/com/taipeihot/jazzlist/LoginActivity.java
+++ src/com/taipeihot/jazzlist/LoginActivity.java
@@ -1,22 +1,63 @@
 package com.taipeihot.jazzlist;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 
 import com.taipeihot.jazzlist.model.Data;
+import com.taipeihot.jazzlist.table.Table;
 
 public class LoginActivity extends Activity {
+
+    AlertDialog registerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initRegisterDialog();
         writeAccount();
         readAccount();
+        new Table(this);
+        connect_to_server();
+    }
+
+    private void initRegisterDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View alert_view = inflater.inflate(R.layout.register_dialog, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(alert_view)
+        .setPositiveButton("Jizz", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // FIRE ZE MISSILES!
+            }
+        })
+        .setNegativeButton("fuck", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        registerDialog = builder.create();
+        //registerDialog = new AlertDialog(this);
+        //registerDialog.setContentView(R.layout.register_dialog);
+        //Window dialogWindow = registerDialog.getWindow();
+        // WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        //lp.copyFrom(registerDialog.getWindow().getAttributes());
+        //lp.y = 100;
+        //lp.height = 600;
+        //dialogWindow.setAttributes(lp);
     }
 
     @Override
@@ -31,22 +72,35 @@ public class LoginActivity extends Activity {
         System.out.println("login");
         Intent intent = new Intent();
         //intent.setClass(LoginActivity.this, MainActivity.class);
-        intent.setClass(LoginActivity.this, LeftAndRightActivity.class);
+        intent.setClass(LoginActivity.this, MainActivity.class);
         System.out.println("meow");
         startActivity(intent);
         System.out.println("meow2");
     }
 
+    public void register(View view) {
+        registerDialog.getWindow().setLayout(600, 400);
+        registerDialog.show();
+        //Window dialogWindow = registerDialog.getWindow();
+        /*WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(registerDialog.getWindow().getAttributes());
+        lp.y = 100;
+        lp.height = 1000;
+        registerDialog.getWindow().setAttributes(lp);*/
+        return;
+
+    }
+
     public void registerLater(View view) {
         Intent intent = new Intent();
         //intent.setClass(LoginActivity.this, MainActivity.class);
-        intent.setClass(LoginActivity.this, LeftAndRightActivity.class);
+        intent.setClass(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
     private void writeAccount(){
         SharedPreferences pre = getSharedPreferences("loginValue",MODE_PRIVATE);
-        String account = "david942j@gmail.com";
+        String account = "david942jizzz@gmail.com";
         String nickname = "david";
         String password = "Hue Nguyen";
         pre.edit().putString("account", account).putString("nickname",nickname).putString("password",Util.MD5(password)).commit();
@@ -57,5 +111,20 @@ public class LoginActivity extends Activity {
         String nickname = sp.getString("nickname", null);
         String password = sp.getString("password", null);
         Data.setAccount(account,nickname,password);
+    }
+    private void connect_to_server(){
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                if(isNetworkAvailable())
+                    CommunicateHelper.start();
+            }
+        });
+        thread.start();
+    }
+    private boolean isNetworkAvailable(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
