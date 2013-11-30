caa57b75a646b6b35d64494b7748731fdf27b4ab
b92paul <b92paul@gmail.com>
2013-11-30 21:13:52 +0800

    register page fin

--- src/com/taipeihot/jazzlist/CategoryFragment.java
+++ src/com/taipeihot/jazzlist/CategoryFragment.java
@@ -1,11 +1,13 @@
  package com.taipeihot.jazzlist;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+@SuppressLint("NewApi")
 public class CategoryFragment extends Fragment {
 	
 	public CategoryFragment(){}
