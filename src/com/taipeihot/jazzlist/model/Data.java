e9936e7efeca8944b4d2169eb35c71b7baafdce9
david942j <david942j@gmail.com>
2013-12-02 11:06:16 +0800

    hotfix

--- src/com/taipeihot/jazzlist/model/Data.java
+++ src/com/taipeihot/jazzlist/model/Data.java
@@ -7,13 +7,12 @@ import com.taipeihot.jazzlist.R;
 import com.taipeihot.jazzlist.table.CategoryTable;
 
 public class Data {
-<<<<<<< HEAD
 	public static String account = null;
 	public static String nickname = null;
 	public static String encryptedPassword = null;
 	public static ArrayList<User> friends = new ArrayList<User>();
 	public static ArrayList<Status> status = new ArrayList<Status>();
-	public static ArrayList<Category> categorys = new ArrayList<Category>();
+	public static ArrayList<Category> categories = new ArrayList<Category>();
 	
 	public static void login(String account, String password){
 		/*setAccount(account,account,Util.MD5(password));
@@ -38,36 +37,14 @@ public class Data {
 	public static boolean addCategory(String title){
 		Category c = new Category(title,R.drawable.ic_home);
 		CategoryTable.insert(c);
-		categorys.add(c);
+		categories.add(c);
 		return CommunicateHelper.addCategory(c);
 	}
+	public static ArrayList<Category> getCategories() {
+        return categories;
+    }
 	public static void addStatus(Status s) {
 		status.add(s);
 		//TODO add to the front? Need a magic sort.
 	}
-=======
-    public static String account = null;
-    public static String nickname = null;
-    public static String encryptedPassword = null;
-    public static ArrayList<User> friends = new ArrayList<User>();
-    public static ArrayList<Status> status = new ArrayList<Status>();
-    public static ArrayList<Category> categories = new ArrayList<Category>();
-    public static void setAccount(String a, String n, String password) {
-        account = a;
-        nickname = n;
-        encryptedPassword = password;
-    }
-    public static boolean addFriend(String account){
-        return CommunicateHelper.addFriend(account);
-    }
-    public static boolean addCategory(String title){
-        Category c = new Category(title,R.drawable.ic_home);
-        CategoryTable.insert(c);
-        categories.add(c);
-        return CommunicateHelper.addCategory(c);
-    }
-    public static ArrayList<Category> getCategories() {
-        return categories;
-    }
->>>>>>> develop
 }
