d878d545ad3c06c56b5fe2e487f91c62e558517d
darkbtf <darkbtf@gmail.com>
2013-12-02 15:40:44 +0800

    jizz

--- src/com/taipeihot/jazzlist/MainActivity.java
+++ src/com/taipeihot/jazzlist/MainActivity.java
@@ -1,18 +1,27 @@
 package com.taipeihot.jazzlist;
 
+import java.util.ArrayList;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.taipeihot.jazzlist.model.Todo;
 
 
 public class MainActivity extends BaseActivity {
 
+	ArrayList<Todo> todoList;
+	
     public MainActivity() {
         super(R.string.left_and_right);
     }
 
+    private void loadData() {
+    	// TODO: load todos & categories from the database
+    }
+    
     @SuppressLint("NewApi")
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,17 +31,9 @@ public class MainActivity extends BaseActivity {
         getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
         setContentView(R.layout.content_frame);
-        //ListFragment
 
         System.out.println("check point 1: ");
-        //getSupportFragmentManager()
-        //.beginTransaction()
-        //.replace(R.id.content_frame, new SampleListFragment())
-        // .commit();
         Fragment fragment = null;
-        //        fragmentTransaction.replace(R.id.content_frame, fragment);
-
-        //setContentView(R.layout.content_frame);
         fragment = new CategoryFragment();
         getSupportFragmentManager()
         .beginTransaction()
@@ -50,4 +51,7 @@ public class MainActivity extends BaseActivity {
         System.out.println("check point 3: ");
     }
 
+    void changeCategory(int Categoryid) {
+    	
+    }
 }
