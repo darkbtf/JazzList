990e371d521aec45f6313c4135f9cbe31a6ea60f
darkbtf <darkbtf@gmail.com>
2014-01-10 23:12:49 +0800

    meow

--- src/Action.java
+++
@@ -1,5 +0,0 @@
-package com.taipeihot.jazzlist.actions;
-
-class Action {
-	
-}
--- src/com/taipeihot/jazzlist/fight/Action.java
+++
@@ -1,7 +0,0 @@
-package com.taipeihot.jazzlist.fight;
-
-public interface Action {
-	public void exec();
-	public Type getType();
-	public int getId();
-}
