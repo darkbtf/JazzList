7ee8d8704eedc90197c7627ecb2f609773a9262e
darkbtf <darkbtf@gmail.com>
2014-01-14 00:19:40 +0800

    zxc

--- src/com/taipeihot/jazzlist/fight/FightData.java
+++ src/com/taipeihot/jazzlist/fight/FightData.java
@@ -2,10 +2,13 @@ package com.taipeihot.jazzlist.fight;
 
 public class FightData {
 	
-	final static int IDLE = 0;
+	final static int NONE = 0;
 	final static int INVITED = 1;
 	final static int STARTED = 2;
-	final static int DONE = 3;
+	final static int IDLE = 3;
+	final static int DONE = 4;
+	final static int UPDATED = 5;
+	final static int ENDED = 6;
 	
 	static int status = 0;
 	static int inviterId;
@@ -16,17 +19,23 @@ public class FightData {
 	
 	static public boolean isInvited() { return status == INVITED; }
 	static public boolean isStarted() { return status == STARTED; }
+	static public boolean isIdle() { return status == IDLE; }
 	static public boolean isDone() { return status == DONE; }
+	static public boolean isUpdated() { return status == UPDATED; }
+	static public boolean isEnded() { return status == ENDED; }
 
 	static public int getInviterId() { return inviterId; }
 	static public String getInviterNickname() { return inviterNickname; }
 	static public Player getMe() { return me; }
 	static public Player getOpponent() { return opponent ; }
 	
-	
-	static public void setInvited(boolean inv) { status = (inv ? INVITED : 0); }
-	static public void setStarted(boolean fight) { status = (fight ? STARTED : 0); }
-	static public void setDone(boolean done) { status = (done ? DONE : 0); }
+	static public void reset() { status = 0; }
+	static public void setInvited() { status = INVITED; }
+	static public void setStarted() { status = STARTED; }
+	static public void setIdle() { status = IDLE; }
+	static public void setDone() { status = DONE; }
+	public static void setUpdated() { status = UPDATED; }
+	static public void setEnded() { status = ENDED; }
 	
 	static public void setInviterId(int inv) { inviterId = inv; } 
 	static public void setInviterNickname(String inv) { inviterNickname = inv; } 
