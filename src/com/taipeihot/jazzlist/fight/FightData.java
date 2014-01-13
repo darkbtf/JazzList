cec1c02dd2718422ff4f7c13272273a5fe614c2c
darkbtf <darkbtf@gmail.com>
2014-01-13 22:59:27 +0800

    fuck

--- src/com/taipeihot/jazzlist/fight/FightData.java
+++ src/com/taipeihot/jazzlist/fight/FightData.java
@@ -5,11 +5,15 @@ public class FightData {
 	final static int NONE = 0;
 	final static int INVITED = 1;
 	final static int STARTED = 2;
+<<<<<<< HEAD
 	final static int PREPARED = 3;
 	final static int IDLE = 4;
 	final static int DONE = 5;
 	final static int UPDATED = 6;
 	final static int ENDED = 7;
+=======
+	final static int DONE = 3;
+>>>>>>> fuck
 	
 	static int status = 0;
 	static int inviterId;
@@ -20,16 +24,21 @@ public class FightData {
 	
 	static public boolean isInvited() { return status == INVITED; }
 	static public boolean isStarted() { return status == STARTED; }
+<<<<<<< HEAD
 	static public boolean isPrepared() { return status == PREPARED; }
 	static public boolean isIdle() { return status == IDLE; }
 	static public boolean isDone() { return status == DONE; }
 	static public boolean isUpdated() { return status == UPDATED; }
 	static public boolean isEnded() { return status == ENDED; }
+=======
+	static public boolean isDone() { return status == DONE; }
+>>>>>>> fuck
 
 	static public int getInviterId() { return inviterId; }
 	static public String getInviterNickname() { return inviterNickname; }
 	static public Player getMe() { return me; }
 	static public Player getOpponent() { return opponent ; }
+<<<<<<< HEAD
 	
 	static public void reset() { status = 0; }
 	static public void setInvited() { status = INVITED; }
@@ -39,6 +48,13 @@ public class FightData {
 	static public void setDone() { status = DONE; }
 	public static void setUpdated() { status = UPDATED; }
 	static public void setEnded() { status = ENDED; }
+=======
+	
+	
+	static public void setInvited(boolean inv) { status = (inv ? INVITED : 0); }
+	static public void setStarted(boolean fight) { status = (fight ? STARTED : 0); }
+	static public void setDone(boolean done) { status = (done ? DONE : 0); }
+>>>>>>> fuck
 	
 	static public void setInviterId(int inv) { inviterId = inv; } 
 	static public void setInviterNickname(String inv) { inviterNickname = inv; } 
--- src/com/taipeihot/jazzlist/jazzmon/FightActivity.java
+++ src/com/taipeihot/jazzlist/jazzmon/FightActivity.java
@@ -3,7 +3,6 @@ package com.taipeihot.jazzlist.jazzmon;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,176 +15,104 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
-import com.taipeihot.jazzlist.MainActivity;
 import com.taipeihot.jazzlist.R;
 import com.taipeihot.jazzlist.adapter.ItemListAdapter;
-
 import com.taipeihot.jazzlist.adapter.ActionListAdapter;
-
 import com.taipeihot.jazzlist.model.Action;
 import com.taipeihot.jazzlist.model.Data;
 import com.taipeihot.jazzlist.fight.FightData;
 import com.taipeihot.jazzlist.fight.Player;
-import com.taipeihot.jazzlist.Util;
 
 public class FightActivity extends Activity {
+        ActionListAdapter actionListAdapter;
+        ArrayList<Action> actionItems=new ArrayList<Action>();
+        ItemListAdapter itemListAdapter;
+        ArrayList<Action> itemItems=new ArrayList<Action>();
+        Player me;
+        Player opponent;
+        Thread fightThread;
+        private String menuName[]={"Skills","Items"};
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_fight);
+                actionItems=Data.getAvailableSkills();
+                itemItems=Data.getItems();
+                
+                ImageView img = (ImageView)findViewById(R.id.fight_enemy_animation);
+                img.setBackgroundResource(R.drawable.fire0);
 
-	ActionListAdapter actionListAdapter;
+                 // Get the background, which has been compiled to an AnimationDrawable object.
+                AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
 
-	ArrayList<Action> actionItems=new ArrayList<Action>();
-	ItemListAdapter itemListAdapter;
-	ArrayList<Action> itemItems=new ArrayList<Action>();
-	Player me;
-	Player opponent;
-	Thread fightThread;
-	private String menuName[]={"Skills","Items"};
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		Util.errorReport("gua4: " + (FightData.isPrepared() ? "PREPARED!" : "NOT PREPARED"));
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fight);
-		actionItems=Data.getAvailableSkills();
-		itemItems=Data.getItems();
-		
-		ImageView img = (ImageView)findViewById(R.id.fight_enemy_animation);
-		img.setBackgroundResource(R.drawable.fire0);
+                 // Start the animation (looped playback by default).
+                frameAnimation.start();
+                ImageView img2 = (ImageView)findViewById(R.id.fight_self_animation);
+                img2.setBackgroundResource(R.drawable.water2);
 
-		 // Get the background, which has been compiled to an AnimationDrawable object.
-		AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+                 // Get the background, which has been compiled to an AnimationDrawable object.
+                AnimationDrawable frameAnimation2 = (AnimationDrawable) img2.getBackground();
 
-		 // Start the animation (looped playback by default).
-		frameAnimation.start();
-		/*ImageView img2 = (ImageView)findViewById(R.id.fight_self_animation);
-		img2.setBackgroundResource(R.drawable.water2);
+                 // Start the animation (looped playback by default).
+                frameAnimation2.start();
+                
+                final GridView skillList=(GridView)findViewById(R.id.skill_gridview);
+                actionListAdapter = new ActionListAdapter(this, actionItems);
+                skillList.setAdapter(actionListAdapter);
+                itemListAdapter = new ItemListAdapter(this, itemItems);
+                
+                
+                ListView menuList=(ListView)findViewById(R.id.fight_menu);
+                menuList.setAdapter(new ArrayAdapter<String>(
+                                this,android.R.layout.simple_list_item_1 , menuName));
+                menuList.setOnItemClickListener(new OnItemClickListener() {
+                        public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                                if(position==0)
+                                {
+                                        skillList.setAdapter(actionListAdapter);
+                                }
+                                else if(position==1)
+                                {
+                                        skillList.setAdapter(itemListAdapter);
+                                }
+                        }
+                });
+                
+                fightThread = new Thread(new Runnable(){
 
-		 // Get the background, which has been compiled to an AnimationDrawable object.
-		AnimationDrawable frameAnimation2 = (AnimationDrawable) img2.getBackground();
-
-		 // Start the animation (looped playback by default).
-		frameAnimation2.start();*/
-		
-		final GridView skillList=(GridView)findViewById(R.id.skill_gridview);
-		actionListAdapter = new ActionListAdapter(this, actionItems);
-		skillList.setAdapter(actionListAdapter);
-		itemListAdapter = new ItemListAdapter(this, itemItems);
-		
-		
-		ListView menuList=(ListView)findViewById(R.id.fight_menu);
-		menuList.setAdapter(new ArrayAdapter<String>(
-				this,android.R.layout.simple_list_item_1 , menuName));
-		menuList.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-				if(position==0)
-				{
-					skillList.setAdapter(actionListAdapter);
-				}
-				else if(position==1)
-				{
-					skillList.setAdapter(itemListAdapter);
-				}
-			}
-		});
-
-		final ProgressBar oppoHpBar =((ProgressBar) findViewById(R.id.fight_enemy_hp));
-		final ProgressBar oppoMpBar = ((ProgressBar) findViewById(R.id.fight_enemy_mp));
-		
-		Thread XD = new Thread(new Runnable(){
-			@Override
-			public void run(){
-				runOnUiThread(new Runnable(){
-					@Override
-					public void run(){
-						Util.errorReport("mewoQQQQQya");
-						ImageView img2 = (ImageView)findViewById(R.id.fight_self_animation);
-						img2.setBackgroundResource(R.drawable.fire2);
-
-						 // Get the background, which has been compiled to an AnimationDrawable object.
-						AnimationDrawable frameAnimation2 = (AnimationDrawable) img2.getBackground();
-
-						 // Start the animation (looped playback by default).
-						frameAnimation2.start();
-						((ProgressBar) findViewById(R.id.fight_self_hp)).setMax(100);
-						((ProgressBar) findViewById(R.id.fight_self_hp)).setProgress(100);
-						((ProgressBar) findViewById(R.id.fight_self_mp)).setMax(100);
-						((ProgressBar) findViewById(R.id.fight_self_mp)).setProgress(100);
-						Util.errorReport("zzz");
-					}
-				});
-			}
-		});
-		//XD.start();
-		
-		fightThread = new Thread(new Runnable(){
-
-			@Override
-			public void run() {
-				while (true) {
-					if (FightData.isPrepared() || FightData.isUpdated()) {
-						me = FightData.getMe();
-						opponent = FightData.getOpponent();
-						runOnUiThread(new Runnable() {
-							@Override
-							public void run() {
-								Util.errorReport("meow "+((ProgressBar) findViewById(R.id.fight_enemy_hp)).getMax());
-								((ProgressBar) findViewById(R.id.fight_enemy_hp)).setMax(opponent.getHp());
-								((ProgressBar) findViewById(R.id.fight_enemy_hp)).setProgress(opponent.getHp());
-								((ProgressBar) findViewById(R.id.fight_enemy_mp)).setMax(opponent.getMp());
-								((ProgressBar) findViewById(R.id.fight_enemy_mp)).setProgress(opponent.getMp());
-								Util.errorReport("meow2 "+((ProgressBar) findViewById(R.id.fight_enemy_hp)).getMax());
-								((ProgressBar) findViewById(R.id.fight_self_hp)).setMax(me.getHp());
-								((ProgressBar) findViewById(R.id.fight_self_hp)).setProgress(me.getHp());
-								((ProgressBar) findViewById(R.id.fight_self_mp)).setMax(me.getMp());
-								((ProgressBar) findViewById(R.id.fight_self_mp)).setProgress(me.getMp());
-							}
-						});
-						FightData.setIdle();
-					} else if (FightData.isEnded()) {
-						FightData.reset();
-						Intent intent = new Intent(FightActivity.this, MainActivity.class);
-						startActivity(intent);
-					}
-					try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				
-			}
-			
-		});
-		fightThread.start();
-		
-		
-	}
-	
-	public void refreshBars() {
-
-		/*runOnUiThread*/
-		/*findViewById(android.R.id.content).post(new Runnable() {
-			public void run() {
-				Util.errorReport("QQQQQQ");
-				Util.errorReport(((ProgressBar) findViewById(R.id.fight_enemy_hp)).getMax() + "");
-				oppoHpBar.setMax(opponent.getHp());
-				Util.errorReport("QQQQQQ");
-				oppoHpBar.setProgress(opponent.getHp());
-				Util.errorReport("QQQQQQ");
-				oppoMpBar.setMax(opponent.getMp());
-				oppoMpBar.setProgress(opponent.getMp());
-				((ProgressBar) findViewById(R.id.fight_self_hp)).setMax(me.getHp());
-				((ProgressBar) findViewById(R.id.fight_self_hp)).setProgress(me.getHp());
-				((ProgressBar) findViewById(R.id.fight_self_mp)).setMax(me.getMp());
-				((ProgressBar) findViewById(R.id.fight_self_mp)).setProgress(me.getMp());
-				//((ProgressBar) findViewById(R.id.fight_self_mp)).
-			}
-		});*/
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.fight, menu);
-		return true;
-	}
+                        @Override
+                        public void run() {
+                                while (true) {
+                                        
+                                        try {
+                                                Thread.sleep(500);
+                                        } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                        }
+                                }
+                        }
+                        
+                });
+                fightThread.start();
+                me = FightData.getMe();
+                opponent = FightData.getOpponent();
+                ((ProgressBar) findViewById(R.id.fight_enemy_hp)).setMax(opponent.getHp());
+                ((ProgressBar) findViewById(R.id.fight_enemy_hp)).setProgress(opponent.getHp());
+                ((ProgressBar) findViewById(R.id.fight_enemy_mp)).setMax(opponent.getMp());
+                ((ProgressBar) findViewById(R.id.fight_enemy_mp)).setProgress(opponent.getMp());
+                ((ProgressBar) findViewById(R.id.fight_self_hp)).setMax(me.getHp());
+                ((ProgressBar) findViewById(R.id.fight_self_hp)).setProgress(me.getHp());
+                ((ProgressBar) findViewById(R.id.fight_self_mp)).setMax(me.getMp());
+                ((ProgressBar) findViewById(R.id.fight_self_mp)).setProgress(me.getMp());
+                
+                
+        }
+        
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.fight, menu);
+                return true;
+        }
 
 }
