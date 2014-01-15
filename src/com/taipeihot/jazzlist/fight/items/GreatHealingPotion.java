8922739b785afc43cefafa2c5a75ba5534565ccd
darkbtf <darkbtf@gmail.com>
2014-01-15 14:22:34 +0800

    zxc

--- src/com/taipeihot/jazzlist/fight/items/GreatHealingPotion.java
+++ src/com/taipeihot/jazzlist/fight/items/GreatHealingPotion.java
@@ -4,7 +4,11 @@ import com.taipeihot.jazzlist.fight.FightAction;
 import com.taipeihot.jazzlist.fight.Player;
 
 public class GreatHealingPotion extends FightAction {
-
+	
+	public GreatHealingPotion(String selfAnime, String opponentAnime) {
+		super(selfAnime, opponentAnime);
+	}
+	
 	@Override
 	public String getMessage(Player player1, Player player2) {
 		// TODO Auto-generated method stub
