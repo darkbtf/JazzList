22516709b0e461e4b6ba9d01fbf2635ad4556bb3
b92paul <b92paul@gmail.com>
2014-01-16 02:31:10 +0800

    add reset btn in tree

--- src/com/taipeihot/jazzlist/jazzmon/SkillFragment.java
+++ src/com/taipeihot/jazzlist/jazzmon/SkillFragment.java
@@ -29,6 +29,7 @@ public class SkillFragment extends Fragment {
 	ArrayList<Action> thunderSkills=Data.getThunderSkills();
 	TreeListAdapter treeListAdapter;
 	GridView skillTree;
+	int now;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -36,8 +37,19 @@ public class SkillFragment extends Fragment {
     	init();
     	skillTree=(GridView)view.findViewById(R.id.tree_skill_tree);
     	treeListAdapter=new TreeListAdapter(this,fireSkills);
+    	now=0;
     	skillTree.setAdapter(treeListAdapter);
-    	
+    	ImageButton resetButton=(ImageButton)view.findViewById(R.id.reset_btn);
+    	resetButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				switchSkillSet(0);
+				Data.resetSkillPoints();
+				init();
+				resetSkillSet(now);
+				
+			}
+    	});
     	ImageButton fireButton = (ImageButton) view.findViewById(R.id.fireSkillBtn);
     	fireButton.setOnClickListener(new OnClickListener(){
 			@Override
@@ -65,7 +77,7 @@ public class SkillFragment extends Fragment {
     
     public void switchSkillSet(int skillSetId) {
     	LinearLayout layout = ((LinearLayout) view.findViewById(R.id.layout_skilltree));
-    	
+    	now=skillSetId;
     	if (skillSetId == 0) {
     		layout.setBackground(getResources().getDrawable(R.drawable.firetree));
     		treeListAdapter=new TreeListAdapter(this,fireSkills);
@@ -76,6 +88,26 @@ public class SkillFragment extends Fragment {
         	skillTree.setAdapter(treeListAdapter);
     	} else {
     		layout.setBackground(getResources().getDrawable(R.drawable.thundertree));
+    		treeListAdapter=new TreeListAdapter(this,thunderSkills);
+        	skillTree.setAdapter(treeListAdapter);
+    	}
+    }
+    public void resetSkillSet(int skillSetId) {
+    	LinearLayout layout = ((LinearLayout) view.findViewById(R.id.layout_skilltree));
+    	now=skillSetId;
+    	if (skillSetId == 0) {
+    		layout.setBackground(getResources().getDrawable(R.drawable.firetree));
+    		fireSkills=Data.getFireSkills();
+    		treeListAdapter=new TreeListAdapter(this,fireSkills);
+    		skillTree.setAdapter(treeListAdapter);
+    	} else if (skillSetId == 1) {
+    		layout.setBackground(getResources().getDrawable(R.drawable.watertree));	
+    		fireSkills=Data.getWaterSkills();
+    		treeListAdapter=new TreeListAdapter(this,waterSkills);
+        	skillTree.setAdapter(treeListAdapter);
+    	} else {
+    		layout.setBackground(getResources().getDrawable(R.drawable.thundertree));
+    		fireSkills=Data.getThunderSkills();
     		treeListAdapter=new TreeListAdapter(this,thunderSkills);
         	skillTree.setAdapter(treeListAdapter);
     	}
